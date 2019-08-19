f251f1c9b4b5c613bec0b0a15fe9d24d6157519e
夏冬 <xiadong@tongbanjie.com>
2019-08-19 10:12:46 +0800

    中断测试

---
+++ summer-code/src/main/java/tanaduus/github/io/thread/InterruptTest.java
@@ -0,0 +1,59 @@
+package tanaduus.github.io.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class InterruptTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+//        Thread.currentThread().interrupt();
+
+        Thread t1 = new Thread("t1") {
+
+            @Override
+            public void run(){
+
+                System.out.println("t1:"+Thread.currentThread().isInterrupted());
+                System.out.println("t1 this:"+this.isInterrupted());
+
+                while(true){
+
+                    try {
+                        TimeUnit.SECONDS.sleep(10);
+                        System.out.println("wake up");
+                    } catch (InterruptedException e) {
+                        System.out.println("t1:"+Thread.currentThread().isInterrupted());
+                        System.out.println("t1 this:"+this.isInterrupted());
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+
+
+//                    if(Thread.interrupted()) {
+//                        System.out.println("ok");
+//                    }
+
+                    if(Thread.currentThread().isInterrupted()) {
+                        System.out.println("ok");
+                        break;
+                    }
+                }
+
+                System.out.println("t1:"+Thread.currentThread().isInterrupted());
+                System.out.println("t1 this:"+this.isInterrupted());
+
+            }
+        };
+
+//        System.out.println(Thread.interrupted());
+
+        t1.start();
+
+        TimeUnit.SECONDS.sleep(5);
+
+//        System.out.println(t1.isInterrupted());
+        t1.interrupt();
+//        System.out.println(t1.isInterrupted());
+
+    }
+}
--- summer-code/src/main/java/tanaduus/github/io/thread/threadFactoryBuilder.java
+++ summer-code/src/main/java/tanaduus/github/io/thread/threadFactoryBuilder.java
@@ -1,22 +1,50 @@
 package tanaduus.github.io.thread;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import java.util.concurrent.*;
 
 public class threadFactoryBuilder {
 
     public static void main(String[] args) {
-//        ExecutorService executorService = new ThreadPoolExecutor(2, 2,
-//                0, TimeUnit.SECONDS,
-//                new LinkedBlockingDeque<>(4),new ThreadFactoryBuilder().setNameFormat("test-%d").build());
-
-        //guava实现了format功能,直接实现ThreadFactory未能实现
-        ExecutorService executorService = new ThreadPoolExecutor(2, 2,
+        ExecutorService executorService = new ThreadPoolExecutor(4, 4,
                 0, TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(2), new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                Thread t = new Thread(r, "xia");
-                return t;
+                new LinkedBlockingDeque<>(4),new ThreadFactoryBuilder().setNameFormat("test-%d").build());
+
+
+        Object o = new Object();
+
+        Future<String> submit = executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(20000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "success";
+        });
+
+        submit.cancel(true);
+
+        executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+            synchronized (o){
+                try {
+                    o.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+            synchronized (o){
+                try {
+                    o.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -24,48 +52,24 @@ public class threadFactoryBuilder {
             System.out.println(Thread.currentThread().getName());
             try {
                 Thread.sleep(2000);
+                synchronized (o) {
+                    o.notifyAll();
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
-//        executorService.submit(() -> {
-//            System.out.println(Thread.currentThread().getName());
+//        executorService.shutdown();
+//        while (!executorService.isTerminated()) {
+//            System.out.println("stopping");
 //            try {
 //                Thread.sleep(2000);
 //            } catch (InterruptedException e) {
 //                e.printStackTrace();
 //            }
-//        });
-//
-//        executorService.submit(() -> {
-//            System.out.println(Thread.currentThread().getName());
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//
-//        executorService.submit(() -> {
-//            System.out.println(Thread.currentThread().getName());
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        });
-
-        executorService.shutdown();
-        while (!executorService.isTerminated()) {
-            System.out.println("stopping");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("end");
+//        }
+//        System.out.println("end");
 
     }
 }
