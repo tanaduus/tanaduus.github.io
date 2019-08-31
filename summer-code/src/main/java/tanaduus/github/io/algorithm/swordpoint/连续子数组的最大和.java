9df00cb2da0860faadbf8b1805900bc7acb157fc
xiadong <xiadong@tongbanjie.com>
2019-08-31 16:38:03 +0800

    连续子数组最大和

--- summer-code/src/main/java/tanaduus/github/io/algorithm/swordpoint/连续子数组的最大和.java
+++ summer-code/src/main/java/tanaduus/github/io/algorithm/swordpoint/连续子数组的最大和.java
@@ -9,8 +9,66 @@ package tanaduus.github.io.algorithm.swordpoint;
  */
 public class 连续子数组的最大和 {
 
+    /**
+     * 该题目思路:
+     * f(n)表示结果；
+     * 定义一个数组dp，dp[i]表示以元素array[i]结尾的最大连续子数组和；
+     * 那么：f(n)=max(dp)   (dp长度为n)
+     * <p>
+     * 以[-2,-3,4,-1,-2,1,5,-3]为例
+     * 可以发现,
+     * dp[0] = -2
+     * dp[1] = -3
+     * dp[2] = 4
+     * dp[3] = 3
+     * 以此类推,会发现
+     * dp[i] = max{dp[i-1]+array[i],array[i]}.
+     * <p>
+     * 最后，取dp数组中的最大值
+     *
+     * @param array
+     * @return
+     */
     public int FindGreatestSumOfSubArray(int[] array) {
 
-        return 0;
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+
+        int n = array.length;
+        int[] dp = new int[n];
+
+        int max = array[0];
+
+        for (int i = 0; i < n; i++) {
+            int now = getDp(array, dp, i);
+
+            max = max > now ? max : now;
+        }
+
+
+        return max;
+    }
+
+    private int getDp(int[] array, int[] dp, int index) {
+        if (index == 0) {
+            dp[index] = array[0];
+            return array[0];
+        }
+
+        int r = array[index] + getDp(array, dp, index - 1) > array[index] ?
+                array[index] + getDp(array, dp, index - 1) :
+                array[index];
+
+        dp[index] = r;
+        return r;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {-2, -3, 4, -1, -2, 1, 5, -3};
+
+        连续子数组的最大和 test = new 连续子数组的最大和();
+
+        test.FindGreatestSumOfSubArray(array);
     }
 }
