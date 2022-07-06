6fc62c3aafdcfc04593e83b1f3942c7777cd5c4a
夏冬 <xiadong@dian.so>
2022-07-06 17:08:51 +0800

    leetcode

--- summer-code/src/main/java/tanaduus/github/io/algorithm/二叉树/后序遍历.java
+++ summer-code/src/main/java/tanaduus/github/io/algorithm/二叉树/后序遍历.java
@@ -12,7 +12,7 @@ public class 后序遍历 {
 
     /**
      * 左右中
-     *
+     * <p>
      * left
      * right
      * root
@@ -37,23 +37,41 @@ public class 后序遍历 {
         Stack<TreeNode> st = new Stack<>();
         List<Integer> list = new ArrayList<>();
 
-        TreeNode node = root;
-        st.push(node);
+        st.push(root);
 
-        while (node != null) {
+        TreeNode pre = null;//用来记录已经遍历过的子树
+        while (!st.isEmpty()) {
 
-            if (node.right != null) {
-                st.push(node.right);
-                node = node.right;
-                continue;
+            while (root.left != null) {
+                st.push(root.left);
+                root = root.left;
             }
 
-            if (node.left != null) {
-                st.push(node.left);
-                node = node.left;
-                continue;
+            if (st.isEmpty()) {
+                break;
             }
+            TreeNode node = st.peek();
 
+            if (node.right == null) {
+
+                st.pop();//作为最左子弹出 or 作为右子树弹出
+                list.add(node.val);
+
+                pre = node;//作为右子树弹出，需要记录自己防止再次进入
+
+            } else {
+
+                if (node.right == pre) {
+                    //曾经进入过右子，自己作为根直接弹出
+                    st.pop();
+                    list.add(node.val);
+
+                    pre = node;//作为右子树弹出，需要记录自己防止再次进入
+                } else {
+                    st.push(node.right);
+                    root = node.right;
+                }
+            }
         }
 
         int[] res = new int[list.size()];
