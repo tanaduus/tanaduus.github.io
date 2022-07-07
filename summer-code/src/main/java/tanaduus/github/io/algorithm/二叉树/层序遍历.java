8a385dff6e8cec1435f9e40df0ed3fb190ddcd6d
夏冬 <xiadong@dian.so>
2022-07-07 11:12:01 +0800

    leetcode

---
+++ summer-code/src/main/java/tanaduus/github/io/algorithm/二叉树/层序遍历.java
@@ -0,0 +1,20 @@
+package tanaduus.github.io.algorithm.二叉树;
+
+import java.util.ArrayList;
+
+/**
+ * @author 夏冬
+ * @date 2022/7/6
+ */
+public class 层序遍历 {
+
+    /**
+     * 双队列加size法
+     */
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        // write code here
+
+
+        return null;
+    }
+}
