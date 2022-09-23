cabf48a176ebd36e6b8745742273a9b8cc7924fc
tanaduus <tanaduus@gmail>
2022-09-24 01:07:53 +0800

    leeicode

---
+++ summer-code/src/main/java/tanaduus/github/io/algorithm/leetcode/editor/cn/LongestCommonPrefix.java
@@ -0,0 +1,51 @@
+//编写一个函数来查找字符串数组中的最长公共前缀。 
+//
+// 如果不存在公共前缀，返回空字符串 ""。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：strs = ["flower","flow","flight"]
+//输出："fl"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：strs = ["dog","racecar","car"]
+//输出：""
+//解释：输入不存在公共前缀。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= strs.length <= 200 
+// 0 <= strs[i].length <= 200 
+// strs[i] 仅由小写英文字母组成 
+// 
+//
+// Related Topics 字符串 👍 2471 👎 0
+
+package tanaduus.github.io.algorithm.leetcode.editor.cn;
+/**
+  * 题目Id：14
+  * 题目：最长公共前缀
+  * 日期：2022-09-24 00:00:19
+*/
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        Solution solution = new LongestCommonPrefix().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
